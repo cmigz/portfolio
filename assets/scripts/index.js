bb9854d76e30d0a55c237bb1a5d0b4ee709875d3
cmigz <cmignacca@gmail.com>
2017-09-04 14:52:02 -0400

    MODIFIED: Scroll Functionality / About / Sections
    * assets/scripts/index.js:
    - Created function to smooth scroll to hrefs
    * assets/styles/fonts.scss:
    - Increased font size on jumbo-text-1 & 2s selectors
    * assets/styles/themes.scss:
    - Increased height of jumbo image to fill whole screen on laptop
    - Added border radius to each section div
    - Created Class Selector: .about-img
      - Set width for photo in about me section
    - Created Class Selector: .center-block
      - Used to center about me picutre in block
    * index.html:
    - About Me Section:
      - Removed boiler text and rows,
      - Added personal photo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,14 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+$('a').click(function () {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 500)
+  return false
+})
+
+// $(document.body).animate({
+//   'scrollTop': $('#skills').offset().top
+// }, 2000)
